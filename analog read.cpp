ff170fc6d35741b8997d15ad7acb245ee7f226ae
fahiz <fahizkppullara@gmail.com>
2024-01-08 17:02:21 +0530

    Analog read

---
+++ analog read.cpp
@@ -0,0 +1,11 @@
+int analogValue = 0;
+int ledPin = 13;
+void setup() {
+ pinMode(ledPin, OUTPUT);
+}
+void loop() {
+ analogValue = analogRead(A0);
+ digitalWrite(ledPin, HIGH);
+ delay(analogValue);
+ digitalWrite(ledPin, LOW);
+ d
