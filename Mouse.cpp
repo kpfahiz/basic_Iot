618955cd4ffe17e6bde1a8619581d04baa192223
fahiz <fahizkppullara@gmail.com>
2024-01-13 17:34:06 +0530

    Mouse

---
+++ Mouse.cpp
@@ -0,0 +1,63 @@
+/*
+Mouse
+an arduino sketch using ps2 mouse library
+see: http://www.arduino.cc/playground/ComponentLib/Ps2mouse
+*/
+// mouse library from : http://www.arduino.cc/playground/ComponentLib/Ps2mouse
+#include <ps2.h>
+const int dataPin = 5
+const int clockPin = 6;
+const int xLedPin = 9;
+const int yLedPin = 11;
+onst int mouseRange = 255; // the maximum range of x/y values
+int xPosition = 0; // values incremented and decremented when mouse moves
+int yPosition = 0;
+int xBrightness = 128; // values increased and decreased based on mouse position
+int yBrightness = 128;
+const byte REQUEST_DATA = 0xeb; // command to get data from the mouse
+PS2 mouse(clockPin, dataPin);
+void setup()
+{
+Serial.begin(115200); // note the higher than usual serial speed
+mouseBegin();
+}
+// get a reading from mouse and report it back to the host via the serial line
+void loop()
+{
+char x; // values read from the mouse
+char y;
+byte status;
+// get a reading from the mouse
+mouse.write(REQUEST_DATA); // ask the mouse for data
+mouse.read(); // ignore ack
+status = mouse.read(); // read the mouse buttons
+if(status & 1) // this bit is set if the left mouse btn pressed
+xPosition = 0; // center the mouse x position
+if(status & 2) // this bit is set if the right mouse btn pressed
+yPosition = 0; // center the mouse y position
+x = mouse.read();
+y = mouse.read();
+if( x != 0 || y != 0)
+{
+// here if there is mouse movement
+xPosition = xPosition + x; // accumulate the position
+xPosition = constrain(xPosition,-mouseRange,mouseRange);
+xBrightness = map(xPosition, -mouseRange, mouseRange, 0,255);
+analogWrite(xLedPin, xBrightness);
+yPosition = constrain(yPosition + y, -mouseRange,mouseRange);
+yBrightness = map(yPosition, -mouseRange, mouseRange, 0,255);
+analogWrite(yLedPin, yBrightness);
+}
+}
+void mouseBegin()
+{
+// reset and initialize the mouse
+mouse.write(0xff); // reset
+delayMicroseconds(100);
+mouse.read(); // ack byte
+mouse.read(); // blank
+mouse.read(); // blank
+mouse.write(0xf0); // remote mode
+mouse.read(); // ack
+delayMicroseconds(100);
+}
