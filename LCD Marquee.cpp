157c9a7701c77583f291ebe2b090e94f55123765
fahiz <fahizkppullara@gmail.com>
2024-01-17 08:32:51 +0530

    LCD 5

--- LCD Marquee.cpp
+++ LCD Marquee.cpp
@@ -9,33 +9,33 @@ const int numRows = 2;
 const int numCols = 16;
 void setup()
 {
-// set up the LCD's number of columns and rows:
-lcd.begin(numCols, numRows);
+    // set up the LCD's number of columns and rows:
+    lcd.begin(numCols, numRows);
 }
 void loop()
 {
-marquee("A message too long to fit !");
-delay(1000);
-lcd.clear();
+    marquee("A message too long to fit !");
+    delay(1000);
+    lcd.clear();
 }
 // this function uses scrolling to display a message up to 32 bytes long
-void marquee( char *text)
+void marquee(char *text)
 {
-int length = strlen(text); // the number of characters in the text
-if(length < numCols)
-lcd.print(text);
-else
-{
-int pos;
-for( pos = 0; pos < numCols; pos++)
-lcd.print(text[pos]);
-delay(1000); // allow time to read the first line before scrolling
-while(pos < length)
-{
-lcd.scrollDisplayLeft();
-lcd.print(text[pos]);
-pos = pos + 1;
-delay(300);
-}
-}
+    int length = strlen(text); // the number of characters in the text
+    if (length < numCols)
+        lcd.print(text);
+    else
+    {
+        int pos;
+        for (pos = 0; pos < numCols; pos++)
+            lcd.print(text[pos]);
+        delay(1000); // allow time to read the first line before scrolling
+        while (pos < length)
+        {
+            lcd.scrollDisplayLeft();
+            lcd.print(text[pos]);
+            pos = pos + 1;
+            delay(300);
+        }
+    }
 }
